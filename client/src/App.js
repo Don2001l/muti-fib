3e07df77036a8ad7b7086dcb87452ff0dadfe83c
root <root@Nomad.donco.com>
2020-01-20 13:26:26 -0500

    updated Client App.js message file

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome HCL Fibonacci Calculator observers </h1>
+            <h1 className="App-title">Welcome to the Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Sample Page</Link>
           </header>
